308bd96e8452daf91a302506a6714f2ee70301d1
MaJl4uLLlka <70745048+MaJl4uLLlka@users.noreply.github.com>
2021-05-15 14:16:26 +0300

    Filter
    added FIlter

--- AirLineTicketOffice/AirLineTicketOffice/Filter.cs
+++ AirLineTicketOffice/AirLineTicketOffice/Filter.cs
@@ -1,10 +1,18 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
 namespace AirLineTicketOffice
 {
     public class Filter : INotifyPropertyChanged
     {
+        public List<String> Companies;
+        public Nullable<TimeSpan> Departure_time;
+        public Nullable<TimeSpan> Arrival_time;
+        public Nullable<decimal>  Price;
+        public Nullable<TimeSpan> Duration; // TODO may be destroyed
+        
         public event PropertyChangedEventHandler PropertyChanged;
 
         public void OnPropertyChanged([CallerMemberName] string prop = "")
