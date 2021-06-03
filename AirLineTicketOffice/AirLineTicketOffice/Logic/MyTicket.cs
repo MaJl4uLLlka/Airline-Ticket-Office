716747a0339f448a600196a04617ebbc782b6711
MaJl4uLLlka <70745048+MaJl4uLLlka@users.noreply.github.com>
2021-06-04 01:37:04 +0300

    Reserve

---
+++ AirLineTicketOffice/AirLineTicketOffice/Logic/MyTicket.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using AirLineTicketOffice.Annotations;
+
+namespace AirLineTicketOffice.Logic
+{
+    public class MyTicket:INotifyPropertyChanged
+    {
+        private string _airline;
+        private string _departureCity;
+        private string _arrivalCity;
+        private DateTime? _departureDate;
+        private TimeSpan? _departureTime;
+        private DateTime? _arrivalDate;
+        private TimeSpan? _arrivalTime;
+        private string _serviceClass;
+        private int? _place;
+
+        public int? Place
+        {
+            get => _place;
+            set
+            {
+                if (value == _place) return;
+                _place = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string ServiceClass
+        {
+            get => _serviceClass;
+            set
+            {
+                if (value == _serviceClass) return;
+                _serviceClass = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Airline
+        {
+            get => _airline;
+            set
+            {
+                if (value == _airline) return;
+                _airline = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string DepartureCity
+        {
+            get => _departureCity;
+            set
+            {
+                if (value == _departureCity) return;
+                _departureCity = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string ArrivalCity
+        {
+            get => _arrivalCity;
+            set
+            {
+                if (value == _arrivalCity) return;
+                _arrivalCity = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public DateTime? DepartureDate
+        {
+            get => _departureDate;
+            set
+            {
+                if (value.Equals(_departureDate)) return;
+                _departureDate = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public TimeSpan? DepartureTime
+        {
+            get => _departureTime;
+            set
+            {
+                if (value.Equals(_departureTime)) return;
+                _departureTime = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public DateTime? ArrivalDate
+        {
+            get => _arrivalDate;
+            set
+            {
+                if (value.Equals(_arrivalDate)) return;
+                _arrivalDate = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public TimeSpan? ArrivalTime
+        {
+            get => _arrivalTime;
+            set
+            {
+                if (value.Equals(_arrivalTime)) return;
+                _arrivalTime = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- AirLineTicketOffice/AirLineTicketOffice/ViewModels/MyTicketsViewModel.cs
+++ AirLineTicketOffice/AirLineTicketOffice/ViewModels/MyTicketsViewModel.cs
@@ -1,7 +1,90 @@
-﻿namespace AirLineTicketOffice.ViewModels
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using AirLineTicketOffice.Annotations;
+using AirLineTicketOffice.Logic;
+using AirLineTicketOffice.Model;
+using AirLineTicketOffice.View;
+
+namespace AirLineTicketOffice.ViewModels
 {
-    public class MyTicketsViewModel
+    public class MyTicketsViewModel:INotifyPropertyChanged
     {
+        public MyTicketsViewModel()
+        {
+            var current_account = MainWindow.db.Accounts.First(u =>
+                u.Passengers.First() != null && u.Passengers.First().name == CurrentUser.Name &&
+                u.Passengers.First().surname == CurrentUser.Surname);
+
+            Passenger current_passenger = current_account.Passengers.First();
+
+            List<Ticket> tickets = current_passenger.Tickets.ToList();
+
+            List<Place> places = new List<Place>();
+
+            foreach (var ticket in tickets)
+            {
+                places.Add(ticket.Place);
+            }
+
+            List<Flight> flights = new List<Flight>();
+
+            foreach (var place in places)
+            {
+                flights.Add(place.Flight);
+            }
+
+            List<Airline> airlines = new List<Airline>();
+
+            foreach (var flight in flights)
+            {
+                airlines.Add(flight.Airline);
+            }
+            
+            List<>
+
+            for (int i = 0; i < tickets.Count(); i++)
+            {
+                
+            }
+
+            _myTickets = new ObservableCollection<MyTicket>();
+
+            for(int i =0; i<tickets.Count();i++ )
+            {
+                _myTickets.Add(new MyTicket
+                    {
+                        DepartureDate = tickets[i].departure_date,
+                        DepartureTime = tickets[i].departure_time,
+                        Place = places[i].place_number,
+                        ServiceClass = places[i].service_class,
+                        
+                    }
+                        );
+            }
+        }
         
+        private ObservableCollection<MyTicket> _myTickets;
+
+        public ObservableCollection<MyTicket> MyTickets
+        {
+            get => _myTickets;
+            set
+            {
+                if (Equals(value, _myTickets)) return;
+                _myTickets = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
